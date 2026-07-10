fabf8ca3030bfaed1b85729573b8584b050d1a6c
Letícia Tchakerian <leticiatchak@gmail.com>
2026-07-10 14:37:34 -0300

    Main Novas alterações

--- tests/test_organizador.sh
+++ tests/test_organizador.sh
@@ -1,31 +1,74 @@
 #!/bin/bash
 
+clear
+
+VERMELHO='\033[1;31m'
+AZUL='\033[1;36m'
+NC='\033[0m'
+
+ERROS=0
+
 # Diretório do teste
 dir_test="/tmp/test_organizador"
 test_hist="../logs/historico_teste.log"
 test_manif="../logs/ultima_operacao_teste.tsv"
 
+rm $dir_test -rf &> /dev/null
+
 # Cria diretório do teste
 mkdir $dir_test
 
 # Cria os arquivos de teste
-echo "teste com extensão .txt" > "$dir_test/anotacoes.txt"
-echo "teste com extensão .html" > "$dir_test/pagina.html"
+echo "teste-txt" > "$dir_test/anotacoes.txt"
+echo "teste-html" > "$dir_test/pagina.html"
 
 # Teste de organizacao dos arquivos
-../scripts/organizar.sh $dir_test $test_hist $test_manif
+NOME_TESTE="Teste de organização em diretórios"
 
-echo -e "Esperado: arquivos organizados em diretórios"
+../scripts/organizar.sh $dir_test $test_hist $test_manif &> /dev/null
 
 if [[ -f "$dir_test/txt/anotacoes.txt" && -f "$dir_test/html/pagina.html" ]]; then
-    echo -e "SUCESSO: Os arquivos foram organizados!"
+    echo -e "$NOME_TESTE [${AZUL}SUCESSO${NC}]"
 else
-    echo "ERRO: Os arquivos não foram organizados."
+    ERROS+=1
+    echo -e "$NOME_TESTE [${VERMELHO}FALHOU${NC}]"
 fi
 
+# Teste do conteúdo dos arquivos
+NOME_TESTE="Teste de conteúdo dos arquivos"
+
+CONTEUDO_TXT=$(cat $dir_test/txt/anotacoes.txt)
+CONTEUDO_HTML=$(cat $dir_test/html/pagina.html)
+
+if [ "$CONTEUDO_TXT" == "teste-txt" ] && [ "CONTEUDO_HTML"=="teste-html" ]; then
+    echo -e "$NOME_TESTE [${AZUL}SUCESSO${NC}]"
+else
+    ERROS+=1
+    echo -e "$NOME_TESTE [${VERMELHO}FALHOU${NC}]"
+fi
+
+
+
+
 # Teste de desfazer organizacao dos arquivos
+NOME_TESTE="Teste de desfazer organização em diretórios"
+
+../scripts/desfazer.sh &> /dev/null
+
+if [[ -f "$dir_test/anotacoes.txt" && -f "$dir_test/pagina.html" ]]; then
+    echo -e "$NOME_TESTE [${AZUL}SUCESSO${NC}]"
+else
+    ERROS+=1
+    echo -e "$NOME_TESTE [${VERMELHO}FALHOU${NC}]"
+fi
 
 
+# Resultado final
+if [[ ERROS -eq 0 ]]; then
+    echo -e "${AZUL}Todos os testes foram bem sucedidos${NC}\n"
+else
+    echo -e "${VERMELHO}Falha em um ou mais testes${NC}\n"
+fi
 
 # Limpa arquivos do teste
 rm $dir_test -rf
